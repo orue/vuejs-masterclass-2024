ef2bbd71692a18d91e750b1c0dc55a9c801f03ef
Carlos Orue <carlos@orue.io>
2025-02-15 13:55:23 -0600

    batman

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import { vueTsConfigs } from '@vue/eslint-config-typescript'
+import pluginVue from 'eslint-plugin-vue'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+  ...pluginVue.configs['flat/essential'],
+  {
+    rules: {
+      'vue/multi-word-component-names': 0,
+    },
+  },
+  ...vueTsConfigs(),
+  skipFormatting,
+]
